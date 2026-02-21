b53b88f312b2b52c755e757b2ed10406445b46d1
recepkeser43-pixel <recepkeser43@gmail.com>
2026-02-22 01:36:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import http from 'http';
 const TOKEN = '7990998595:AAEeC6KINLvSYEiOuVV1rL_VJNq_pH7MSAg';
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// Render'ı kandırmak için kapı açıyoruz
+// Render'ın beklediği kapıyı (port) açıyoruz
 http.createServer((req, res) => {
   res.writeHead(200);
   res.end('Bot Aktif');
 }).listen(process.env.PORT || 8080);
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "✅ SONUNDA! Sistem çalışıyor Recep. Şimdi mühürleri ekleyebiliriz.");
+  bot.sendMessage(msg.chat.id, "✅ BAŞARILI: Bot ve paketler sonunda hazır!");
 });
 
-console.log("Bot başlatıldı...");
+console.log("Bot calisiyor...");
