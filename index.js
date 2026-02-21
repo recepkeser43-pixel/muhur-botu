cd45cd9657c5fe187b88df80b5b846b0c3277fca
recepkeser43-pixel <recepkeser43@gmail.com>
2026-02-22 01:41:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import TelegramBot from 'node-telegram-bot-api';
-import http from 'http';
+const TelegramBot = require('node-telegram-bot-api');
+const http = require('http');
 
 const TOKEN = '7990998595:AAEeC6KINLvSYEiOuVV1rL_VJNq_pH7MSAg';
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// Render'ın beklediği kapıyı (port) açıyoruz
+// Render'ın beklediği port kapısı
 http.createServer((req, res) => {
   res.writeHead(200);
   res.end('Bot Aktif');
 }).listen(process.env.PORT || 8080);
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "✅ BAŞARILI: Bot ve paketler sonunda hazır!");
+  bot.sendMessage(msg.chat.id, "✅ SONUNDA OLDU! Sistem bu sefer sağlam kuruldu Recep.");
 });
 
-console.log("Bot calisiyor...");
+console.log("Bot eski tip bağlantı ile başlatıldı...");
